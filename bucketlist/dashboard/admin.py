67ec2332a5396d26c9d20ea4e21ace0f44f0045c
Uzo Awili <uzo.awili@andela.com>
2015-11-23 01:59:36 +0100

    (feature) added script and classes for django admin backend

--- bucketlist/dashboard/admin.py
+++ bucketlist/dashboard/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+# from django.db.models import Count
+from models import BucketList, BucketListItem
 
-# Register your models here.
+
+class BucketListItemAdminInLine(admin.TabularInline):
+    """
+    Admin definiton object for the BucketListItem model
+    """
+    model = BucketListItem
+    fields = ('name', 'description', 'done')
+    fk_name = 'bucketlist'
+    extra = 0
+
+
+class BucketListAdmin(admin.ModelAdmin):
+    """
+    Admin definiton object for the BucketList model
+    """
+    fieldsets = [
+        (None, {
+            "fields": ('name', 'description','creator'),   
+        }),
+    ]
+    readonly_fields = ('creator',)
+    search_fields = ('name',)
+    list_display = ('name', 'description','creator', 'date_created', 'date_modified',)
+    inlines = [BucketListItemAdminInLine,]
+
+
+# registers model admin classes:
+admin.site.register(BucketList, BucketListAdmin)
