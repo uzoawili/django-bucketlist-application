213d33df4d417667ee667ad23bc92d9a89c720b5
Uzo Awili <uzo.awili@andela.com>
2015-12-01 09:25:26 +0100

    (chore) added django-postgrespool to requirements

--- bucketlist/bucketlist/settings/staging.py
+++ bucketlist/bucketlist/settings/staging.py
@@ -10,14 +10,14 @@ from .base import *
 
 APPLICATION_DIR = os.path.dirname(globals()['__file__'])
 
-DEBUG = False
+DEBUG = True
 
 DATABASES = {
     'default': dj_database_url.config()
 }
 
 # Enable Connection Pooling
-DATABASES['default']['ENGINE'] = 'django_postgrespool'
+# DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
